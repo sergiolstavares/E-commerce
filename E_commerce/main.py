2bcd4e7157bc10704676bc16eecce0a7e0e7d112
Sérgio <leocapone210@gmail.com>
2021-02-18 15:29:51 -0300

    Implementação das regras do negócio

--- E_commerce/main.py
+++ E_commerce/main.py
@@ -12,14 +12,11 @@ cursor.execute("CREATE TABLE IF NOT EXISTS carrinho (cpf interger, produto text,
 valor_total = []
 produtos_add = []
 
+
 def voltar_pro_login():  # função para voltar pro login de qualquer tela
     cadastro.close()
     cadastro_produto.close()
     tela_principal.close()
-    tela_principal.lista_carrinho.clear()
-    tela_principal.mostrar_valor_total.setText("0")
-    valor_total.clear()
-    produtos_add.clear()
     login.show()
 
 
@@ -59,8 +56,6 @@ def logar():  # função para entrar na tela principal da loja e fechar o login
             5] == senha_digitado:  # compara a senha/login digitados com senha/login do banco
             puxar_produtos()  # Puxa os produtos cadastrados e mostra na tela principal
             login.close()
-
-
         else:
             print("Usúario ou senha incorreta")
             login.msg_error.setText("Usúario ou senha incorreta")
@@ -68,36 +63,53 @@ def logar():  # função para entrar na tela principal da loja e fechar o login
 
 def puxar_produtos():
     tela_principal.show()
-    tela_principal.mostrar_valor_total.setText("0")
+    tela_principal.mostrar_valor_total.setText("R$ 0")
     cursor.execute("SELECT * FROM produtos")
     produtos_lidos = cursor.fetchall()
     tela_principal.lista_produtos.setRowCount(len(produtos_lidos))
     tela_principal.lista_produtos.setColumnCount(4)
 
-    for i in range(0, len(produtos_lidos)):
+    for i in range(0, len(produtos_lidos)):  # percorre os produtos e cria as linhas e colunas da tabela
         for j in range(0, 4):
             tela_principal.lista_produtos.setItem(i, j, QtWidgets.QTableWidgetItem(str(produtos_lidos[i][j])))
 
 
 def adicionar_carrinho():
-
-    linha_atual = tela_principal.lista_produtos.currentRow()
+    linha_atual = tela_principal.lista_produtos.currentRow()  # comando para ler a linha selecionada
     cursor.execute("SELECT id FROM produtos")
     dados_lidos = cursor.fetchall()
     valor_id = dados_lidos[linha_atual][0]
-    cursor.execute("SELECT nome,valor FROM produtos WHERE id=" + str(valor_id))
+    cursor.execute("SELECT nome,valor,qtd FROM produtos WHERE id=" + str(valor_id))
     produto = cursor.fetchall()
     produto_format = format(f" Produto: {produto[0][0]} |  Valor: R$ {produto[0][1]}")
-    valor_total.append(float(produto[0][1]))
+    valor_total.append(float(produto[0][1]))  # adicionar o valor do produto adicionado ao carrinho na lista = valor_total
     produtos_add.append(produto)
 
     print(produto_format)
     print(produtos_add)
     tela_principal.lista_carrinho.addItem(produto_format)
 
-    valor = sum(valor_total)
+    valor = sum(valor_total)  # recebe a soma da lista valor_total
 
-    tela_principal.mostrar_valor_total.setText(str(valor))
+
+    qtd_int = int(produto[0][2]) - 1
+    qtd_nova = str(qtd_int)
+    print(qtd_nova)
+    cursor.execute("UPDATE produtos SET qtd = " + qtd_nova + " WHERE id=" + str(valor_id))
+    puxar_produtos()
+    tela_principal.mostrar_valor_total.setText(
+        format(f"R$ {str(valor)}"))  # muda a label para mostrar o valor total atual
+
+
+def finalizar_compra():
+    banco.commit()
+    puxar_produtos()
+    tela_principal.lista_carrinho.clear()
+    tela_principal.mostrar_valor_total.setText("R$ 0")
+    valor_total.clear()
+    produtos_add.clear()
+
+    print("Compra Finalizada")
 
 
 def cadastrar_produto():
@@ -161,8 +173,11 @@ def verificar_vendedor():
         print("Código do vendedor incorreto")
 
 
-app = QtWidgets.QApplication([])
+def sair():
+    exit()
 
+
+app = QtWidgets.QApplication([])
 login = uic.loadUi("Login.ui")  # conversão do .ui para .py
 cadastro = uic.loadUi("cadastro.ui")
 cadastro_produto = uic.loadUi("cadastro_produto.ui")
@@ -174,11 +189,12 @@ cadastro.botao_ja_possuo_cadastro.clicked.connect(ja_possuo_cadastro)
 login.btn_vendedor.clicked.connect(area_vendedor)
 cadastro_produto.botao_voltar_login.clicked.connect(voltar_pro_login)
 login.botao_entrar.clicked.connect(logar)
-tela_principal.botao_sair.clicked.connect(voltar_pro_login)
+tela_principal.botao_sair.clicked.connect(sair)
 cadastro.botao_cadastrar.clicked.connect(cadastrar_usuario)
 cadastro_produto.botao_cadastrar_produto.clicked.connect(cadastrar_produto)
 vendedor.botao_vendedor.clicked.connect(verificar_vendedor)
 tela_principal.btn_adicionar_ao_carrinho.clicked.connect(adicionar_carrinho)
+tela_principal.botao_finalizar_compra.clicked.connect(finalizar_compra)
 
 login.show()
 app.exec()
